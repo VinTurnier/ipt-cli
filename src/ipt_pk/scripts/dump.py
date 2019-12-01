4cc4940810eac6d5dd0738af05cc535f617c9d30
Vincent <vincent@vincents-mbp.home>
2019-12-01 15:41:22 -0500

    updated cli to dump and update container as well

--- src/ipt_pk/scripts/dump.py
+++ src/ipt_pk/scripts/dump.py
@@ -6,7 +6,10 @@ from ipt_pk.scripts import configure
 def production_database():
     config = configure.get_config()
     ipt_models_path = config['GIT REPOSITORIES']['ipt-models']
+    print("Dumping into repo...")
     subprocess.call(f'cd {ipt_models_path}/test_db && ./get-production-db.sh',shell=True)
+    print('Updating database container...')
+    subprocess.call('docker exec  -it ipt_local_db /bin/bash ./update-db.sh',shell=True)
 
 def help():
     print("Usage: ipt start [OPTIONS]")
